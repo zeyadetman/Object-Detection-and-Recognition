1bbadf73a555e100cb3786ac74831e4f7718bd82
zeyadetman <zeyadetman@gmail.com>
2018-04-29 08:40:52 +0200

    save to excel -finished

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import os
 import numpy as np
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from openpyxl import *
 
 directory = os.fsencode("Data set\Training")
+wb = load_workbook(r"results.xlsx")
+rowE = 1
+rowM = 1
+rowT = 1
 
+listPR = []
 #This function iterates through the images in (argument directory)
 #convert each image to greyscale and resize it to 50*50
 #then save the result in directory "Data set\\Trainpre\\"
 def preprocessing(directory):
+    imgNum = 1
     for filename in os.listdir(directory):
         fil = str(os.path.join(directory, filename).decode("utf-8"))
         image = cv2.imread(fil)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray_image = cv2.resize(gray_image,(50,50));
         filename = filename.decode("utf-8")
-        PCAfeatureExtraction(gray_image)
+        PCAfeatureExtraction(gray_image,imgNum)
+        imgNum +=1
         #cv2.imwrite("Data set\\Trainpre\\"+ filename,gray_image)
 
 def isolateObjects(imgPath):
@@ -32,16 +40,30 @@ def isolateObjects(imgPath):
         blob = cv2.bitwise_and(image, image, mask=mask)
         cv2.imwrite("Data set\\Testpre\\"+imgPath+"colourblobs-%d-hue_%03d.png" % (i, peak), blob)
 
-def PCAfeatureExtraction(image):
+def PCAfeatureExtraction(image,num):
+    global rowM
+    global rowE
+    global rowT
+    global listPR
+
     X = np.array(image)
     pca = PCA(n_components=2)
     pca.fit(X)
     #print(pca.explained_variance_ratio_)  
     #print(pca.singular_values_)
-    centered_matrix = X - X.mean(axis=1)[:, np.newaxis] #Subtract the Mean
+    meanVal = X.mean(axis=1)[:, np.newaxis]
+    centered_matrix = X - meanVal #Subtract the Mean
+    sheetTabM = wb['trainingimagesmean']
+    for i in range(1,len(meanVal)):
+        sheetTabM.cell(row=rowM, column=1).value = meanVal[i][0]
+        rowM += 1     
     cov = np.dot(centered_matrix, centered_matrix.T) #Covariance matrix
     eigvals, eigvecs = np.linalg.eig(cov) #Calculate the Eigenvectors and Eigenvalues of the covariance matrix
-    #print(eigvals, eigvecs)
+    sheetTab = wb['eigvecs']   
+    for i in range(1,len(eigvecs),1):
+        for j in range(1,len(eigvecs[i]),1):
+            sheetTab.cell(row=rowE, column=j).value = eigvecs[i][j]
+        rowE += 1
     #Order eigenvectors by eigenvalues, highest to lowest.
     #Choose only the highest P eigenvectors. 
     # Make a list of (eigenvalue, eigenvector) tuples
@@ -50,6 +72,20 @@ def PCAfeatureExtraction(image):
     eig_pairs.sort()
     eig_pairs.reverse()
     # Visually confirm that the list is correctly sorted by decreasing eigenvalues
-    print('Eigenvalues in descending order:')
+    #print('Eigenvalues in descending order:')
+    
     for i in eig_pairs:
-        print(i[0])
+        listPR.append(i[0])
+        #print()
+
+preprocessing(directory)
+sheetTabT = wb['training']
+
+k = 0
+for i in range(1,26):
+    for j in range (2,20):
+            sheetTabT.cell(row=i, column=1).value = int(i/25)+1
+            sheetTabT.cell(row=i, column=j).value = listPR[k]
+            k+=1  
+    
+wb.save("results.xlsx")
